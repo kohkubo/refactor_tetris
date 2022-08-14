0fe86b199381672620e2b9cb9f6c86c688016a73
kohkubo <kohkubo@student.42tokyo.jp>
2022-08-14 22:04:42 +0900

    place_matrix_with_mino -> place_mino_on_matrix

--- ttrs_matrix.h
+++ ttrs_matrix.h
@@ -4,7 +4,7 @@
 #include "tetris.h"
 
 bool can_place_on_matrix(const t_matrix matrix, const t_mino_type *mino_type, t_position pos);
-void place_matrix_with_mino(t_matrix matrix, t_current_mino *mino);
+void place_mino_on_matrix(t_matrix matrix, t_current_mino *mino);
 int lock_down_current_mino(t_tetris *tetris);
 
 #endif
